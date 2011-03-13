e09ef00acae1b6a9192202ef05a115e42d84e189
Toyoaki Oko <chariderpato@gmail.com>
2011-03-14 03:20:45 +0900

    first commit

---
+++ src/com/okolabo/android/teidennotify/Top.java
@@ -0,0 +1,13 @@
+package com.okolabo.android.teidennotify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Top extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
